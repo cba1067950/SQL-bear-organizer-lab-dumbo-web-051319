fd74b9d6df0a9411de6e9ff7bcc460929080c112
Joe Jamsky <cba1067950@yahoo.com>
2019-05-22 18:07:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergea
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "Male", "Brown", "Gross", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "Male", "Brown", "Gross", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "Male", "Brown", "Gross", 1);
-INSERT INTO bears (age, gender, color, temperament, alive) VALUES (1, "Male", "Brown", "Gross", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 1, "Male", "Brown", "Gross", 1);
