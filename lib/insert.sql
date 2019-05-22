6ba51e858936c434f4b53a204e7a9a0607ee003c
Joe Jamsky <cba1067950@yahoo.com>
2019-05-22 18:07:13 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Ch
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "Male", "Brown", "Gross", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 1, "Male", "Brown", "Gross", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 1, "Male", "Brown", "Gross", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "Male", "Brown", "Gross", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "Male", "Brown", "Gross", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "Male", "Brown", "Gross", true);
-INSERT INTO bears (age, gender, color, temperament, alive) VALUES (1, "Male", "Brown", "Gross", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "Male", "Brown", "Gross", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "Male", "Brown", "Gross", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "Male", "Brown", "Gross", 1);
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES (1, "Male", "Brown", "Gross", 1);
